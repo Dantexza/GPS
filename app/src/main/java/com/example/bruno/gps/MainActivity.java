558cb6201864fee56c58990d71ea876aa1dec9f9
Dantexza <b.gabriels@hotmail.com>
2017-12-05 17:33:08 -0200

    manda localização para firebase

--- app/src/main/java/com/example/bruno/gps/MainActivity.java
+++ app/src/main/java/com/example/bruno/gps/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.bruno.gps;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.CountDownTimer;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION =1 ;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final GpsTracker gps = new GpsTracker(this);
         int permissionCheck = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
 
@@ -26,9 +35,7 @@ public class MainActivity extends AppCompatActivity {
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
 
 
-
             } else {
-
 
 
                 ActivityCompat.requestPermissions(this,
@@ -37,5 +44,36 @@ public class MainActivity extends AppCompatActivity {
 
 
             }
-    }
-}}
+
+
+
+
+        }loop();
+    }public void loop(){
+
+        final GpsTracker gps = new GpsTracker(this);
+        final byte testByte = 0;
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                while (testByte == 0) {
+                    try {
+                        Thread.sleep(5000);// Waits for 1 second (1000 milliseconds)
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    Context context = getApplicationContext();
+                    String deviceId = Settings.Secure.getString(context.getContentResolver(),
+                            Settings.Secure.ANDROID_ID);
+                    DatabaseReference myReflat = database.getReference(deviceId + "/location/lat");
+                    DatabaseReference myReflong = database.getReference(deviceId + "/location/long");
+
+                    myReflat.setValue(gps.getLatitude());
+                    myReflong.setValue(gps.getLongitude());
+                    run();
+                }
+            }
+        };myRunnable.run();}
+
+}
